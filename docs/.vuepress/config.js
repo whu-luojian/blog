a7ba1b9b65125013d7397608561fe2d705dc0fbe
luojian <1499596970@qq.com>
2021-01-12 17:15:18 +0800

    docs: add engineering-snippet

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -102,7 +102,7 @@ module.exports = {
             'webpack',
             'npm',
             'yarn',
-            'engineering-other'
+            'engineering-snippet'
           ]
         },
         {
